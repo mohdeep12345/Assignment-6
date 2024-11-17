5744f39705d827449bcc365347f4854607309e1a
mohdeep12345 <mohdeepsingh10@gmail.com>
2024-11-17 13:14:29 -0500

    Fixed some text content of server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 /********************************************************************************* 
 
-WEB322 – Assignment 03 
+WEB322 – Assignment 04
 
 I declare that this assignment is my own work in accordance with Seneca
 Academic Policy. No part of this assignment has been copied manually or 
@@ -12,8 +12,8 @@ the course.
 Name:   Mohdeep Singh
 Student ID:   109600239
 Date:  03 November 2024
-Cyclic Web App URL:  https://assignment-3-2-6dht.onrender.com
-GitHub Repository URL:  https://github.com/mohdeep12345/Assignment-3.2.git
+Cyclic Web App URL:  https://assignment-4-mkan.onrender.com
+GitHub Repository URL: https://github.com/mohdeep12345/Assignment-4.git
 
 ********************************************************************************/ 
 
